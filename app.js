de67f95dd34e5671ac8575057249191036e43e55
Hossein Douzandeh <douzandeh.k@gmail.com>
2026-06-08 19:58:26 +0330

    feat: add Cart class and basic cart functionality - Create Cart class in models/Cart.js - Integrate cart with Products.js (add product to cart) - Initialize Cart instance in app.js and pass to Products - Add cart section to HTML

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+import Cart from "./models/Cart.js";
 import Products from "./models/Products.js";
 import { fetchData } from "./utils/httpReq.js";
 
 const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart_list");
+const totalPriceNode = document
+  .getElementById("total_price")
+  .querySelector("span");
 
 async function render() {
   const productsData = await fetchData();
-  const productsInstance = new Products(productsNode, productsData);
+
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const productsInstance = new Products(
+    productsNode,
+    productsData,
+    cartInstance,
+  );
+  console.log(cartInstance);
 
   productsInstance.showProducts();
 }
